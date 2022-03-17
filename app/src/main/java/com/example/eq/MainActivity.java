de5278a4015fb2dce765e5711f1dd62fe1e308d6
Puass <52569705+Puass@users.noreply.github.com>
2022-03-18 02:27:48 +0300

    Firebase

--- app/src/main/java/com/example/eq/MainActivity.java
+++ app/src/main/java/com/example/eq/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.eq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference("message");
+
+        mDatabase.setValue("Hello, World!");
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buton();
+            }
+        });
+    }
+
+    public void buton(){
+
+        TextView text = (TextView)findViewById(R.id.asd);
+        String a = text.getText().toString();
+
+        User user = new User(a, "dasfsadfsa@gmaail.com");
+
+        mDatabase.child("users").child("userId").child("username").setValue(user.username);
+
+
     }
 }
---
+++ app/src/main/java/com/example/eq/User.java
@@ -0,0 +1,18 @@
+package com.example.eq;
+
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
+
